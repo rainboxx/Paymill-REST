7ab457df3f2600e156e7243fd53c2e5a5dfd01cb
Matthias Dietrich <perl@rb.ly>
2013-08-26 00:26:03 +0200

    Initialized

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Paymill::REST - Blah blah blah
+
+# SYNOPSIS
+
+    use Paymill::REST;
+
+# DESCRIPTION
+
+Paymill::REST is
+
+# AUTHOR
+
+Matthias Dietrich <perl@rainboxx.de>
+
+# COPYRIGHT
+
+Copyright 2013- Matthias Dietrich
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Paymill/REST.pm
@@ -0,0 +1,39 @@
+package Paymill::REST;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Paymill::REST - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Paymill::REST;
+
+=head1 DESCRIPTION
+
+Paymill::REST is
+
+=head1 AUTHOR
+
+Matthias Dietrich E<lt>perl@rainboxx.deE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Matthias Dietrich
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
